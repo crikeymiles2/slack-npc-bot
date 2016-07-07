7bfc31bf0dba8198687538760e7079391963af2d
Mike Hutchinson <crikeymiles2@users.noreply.github.com>
2016-07-07 21:57:58 +0100

    Additional comments

--- index.php
+++ index.php
@@ -26,11 +26,12 @@ if($token != 'SAL9CqWkuPZtN7mZnTKKxRZF'){ #replace this with the token from your
   echo $msg;
 }
 
-// Get the NPC Name parts
+# Get the NPC Name parts
 $last_name = file_get_contents('last-names.txt');
 $last_name = explode("\n", $last_name);
 shuffle($last_name);
 
+# Handle the gender argument
 $male = array("male", "man", "boy", "m");
 $female = array("female", "woman", "girl", "f");
 $gender = 0;
@@ -50,11 +51,13 @@ foreach($female as $needle)
     }
 }
 
+# If no gender specified, pick one at random
 if (!$gender) { 
 	if (rand(0, 1)) { $gender = "m"; }
 	else { $gender = "f"; }
 }
 
+# Generate an NPC.
 if ( $gender == "m") {
 	$first_name = file_get_contents('first-names-male.txt');
 	$first_name = explode("\n", $first_name);
